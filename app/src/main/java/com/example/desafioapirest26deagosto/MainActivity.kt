6e78a046c07979e3a3bf1c273eaddb073af9af7a
dieflores <labestia012345>
2020-08-27 10:19:41 -0400

    tercer  commit desafio api rest 26 de agosto

--- app/src/main/java/com/example/desafioapirest26deagosto/MainActivity.kt
+++ app/src/main/java/com/example/desafioapirest26deagosto/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.desafioapirest26deagosto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.desafioapirest26deagosto.modelo.RetroUserRequest
+import com.example.desafioapirest26deagosto.modelo.Users
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Callback<ArrayList<Users>> {
+
+    lateinit var recView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recView = findViewById(R.id.recyclerView)
+        recView.layoutManager = LinearLayoutManager(this)
+
+        loadAPI()
+    }
+
+    fun loadAPI(){
+        var request: RetroUserRequest = RetroUserRequest()
+        request.getUserListRequest(this)
+    }
+
+    override fun onResponse(call: Call<ArrayList<Users>>, response: Response<ArrayList<Users>>) {
+        if (response != null){
+            recView.adapter = UserListAdapter(response.body()!!)
+
+        }
+    }
+
+    override fun onFailure(call: Call<ArrayList<Users>>, t: Throwable) {
+
     }
 }
--- app/src/main/java/com/example/desafioapirest26deagosto/UserListAdapter.kt
+++ app/src/main/java/com/example/desafioapirest26deagosto/UserListAdapter.kt
@@ -1,26 +1,40 @@
 package com.example.desafioapirest26deagosto
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.desafioapirest26deagosto.modelo.Users
 
 class UserListAdapter(var list:ArrayList<Users>):RecyclerView.Adapter<UserListAdapter.Holder>() {
 
     class Holder(itemView: View): RecyclerView.ViewHolder(itemView){
+        lateinit var name: TextView
+        lateinit var email: TextView
+        lateinit var phone: TextView
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        TODO("Not yet implemented")
+        var view:View = LayoutInflater.from(parent.context).inflate(R.layout.user_list_item,parent, false)
+        var holder:Holder= Holder(view)
+        holder.name = view.findViewById(R.id.name)
+        holder.email = view.findViewById(R.id.email)
+        holder.phone = view.findViewById(R.id.phone)
+        return holder
     }
 
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
+
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        TODO("Not yet implemented")
+        holder.name.text = list.get(position).name.toString()
+        holder.phone.text = list.get(position).phone.toString()
+        holder.email.text = list.get(position).email.toString()
+
     }
 }
